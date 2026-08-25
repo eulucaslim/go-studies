499cb0e5e5fd77f366104d20807592dca89b1bb7
Lucas de Lima Canto <lucaslimacnt14@gmail.com>
2026-08-24 23:29:03 -0400

    feat: Trabalhando com requisições em Go pt 2

--- fetch_01_08/main.go
+++ fetch_01_08/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
+
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch this url %v: %v", url, err)
+			os.Exit(1)
+		}
+
+		b, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error fetch url %v: %v", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%v", b)
+	}
+}
